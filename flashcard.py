80ea7b41ccde68ec5adbc53c47535934dc326aa4
airrestic <bilal.safdar085+GitHub@gmail.com>
2026-07-28 09:07:06 +0500

    New file

--- flashcard.py
+++ flashcard.py
@@ -1,1 +1,9 @@
 #The Beginning
+from time import time
+
+
+input("Welcome to the Flashcard app! Press enter to continue.")
+for i in range(3):
+    print("Loading" + "." * i)
+    time.sleep(1)
+    
